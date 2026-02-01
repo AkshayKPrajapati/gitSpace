4cf5951a7d7b343a7a6480781937e724d7d64b27
AkshayKPrajapati <akshayprajapati6200@outlook.com>
2026-02-01 14:51:17 +0530

    11111111111111111111

---
+++ TestConflict.java
@@ -0,0 +1,10 @@
+public class TestConflict {
+    public static void main(String[] args) {
+        System.out.println("This is a test for merge conflict.");
+        System.out.println("Line from branch A");
+        System.out.println("Line from branch A");
+        System.out.println("Line from branch C");
+        System.out.println("Line from branch D");
+        System.out.println("Line from branch E");
+    }
+}
